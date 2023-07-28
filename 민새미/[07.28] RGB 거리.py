36e24f9405647bf29c0e0c4406851e0dfc1ceb8d
Saemi Min <76938304+petcu1004@users.noreply.github.com>
2023-07-28 18:36:21 +0900

    오타 수정

--- 민새미/[07.28] RGB 거리.py
+++ 민새미/[07.28] RGB 거리.py
@@ -9,4 +9,4 @@ for i in range(1,n): # 1부터 시작하는 이유는 다음 입력값이 이전
     arr[i][1]=min(arr[i-1][0], arr[i-1][2]) + arr[i][1]
     arr[i][2]=min(arr[i-1][0], arr[i-1][1]) + arr[i][2]
 
-print(min(a[n-1][0],a[n-1][1],a[n-1][2]))
+print(min(arr[n-1][0],arr[n-1][1],arr[n-1][2]))
